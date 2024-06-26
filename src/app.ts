defb865e5c10e10de0497b8b3ac257342193ace2
otash16 <otajonqutbiddinov1@gmail.com>
2024-06-26 23:09:32 +0500

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 /** 1-ENTRANCE **/
 const app = express();
-console.log(__dirname);
+console.log("app.js");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
-app.use((express.json));
+app.use(express.json());
 /** 2-SESSIONS **/
 
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 /** 4-ROUTERS **/
-
+app.use("/", router);
+app.use("/otajon", router);
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
 import mongoose from "mongoose";
-
+console.log("server.js");
 mongoose.connect(process.env.MONGO_URL as string,{})
 .then((data) =>{
     console.log("MongoDB connection SUCCEED")
-    const PORT = process.env.PORT ??  3000;
+    const PORT = process.env.PORT ??  3003;
     app.listen(PORT, function(){
         console.log(`The server is successfully running on ${PORT}`);
     })
